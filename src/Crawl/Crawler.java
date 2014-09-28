f0896e1126754e15e6baa56b2b2eafda356026a8
Himanshu Pandey <hmp.mcs@gmail.com>
2014-09-28 17:29:54 +0530

    Added comments.
    Switched to omdapi as deanclathworth api was not working.

--- src/Crawl/Crawler.java
+++ src/Crawl/Crawler.java
@@ -22,36 +22,35 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
-import IMDBdataCollector.CollectData;
-
+/**
+ * Uses JSOUP to extract the webpage details.
+ * Uses OMDAPI to get the moview details.
+ * */
 public class Crawler {
 
 	public static void main(String args[]) {
-		// new posTagger().justDoItAlready();
-
-		// Crawler crawl = new Crawler();
-		// System.out.print("Movie name: ");
-		// BufferedReader br = new BufferedReader(new
-		// InputStreamReader(System.in));
-		// String name = null;
-		// try {
-		// name = br.readLine();
-		// } catch (IOException e) {
-		// e.printStackTrace();
-		// }
-		// String url = crawl.generateURL(name);
-		// crawl.retreiveReviewPage(url, name);
-
-		CollectData cd = new CollectData();
-		cd.collectRevies();
+		Crawler imdbMoviewCrawler = new Crawler();
+		String url = imdbMoviewCrawler.generateURL("inception");
+		
+		imdbMoviewCrawler.retreiveReviewPage(url,"inception");
 	}
 
-	public String generateURL(String name) {
+	/**
+	 * Calls the omdapi for getting the movie details. try:-
+	 * http://www.omdbapi.com/?t=inception
+	 * then it checks for imdbID field, which is the imdb id for that movie.
+	 * so the url of the review page will be:-
+	 * "http://www.imdb.com/title/" + id + "/reviews"
+	 * */
+	public String generateURL(String movieName) {
 		try {
+			String url = "http://www.omdbapi.com/?t=" + movieName;
+			String id = "";
+			
 			System.out.println("Generating URL");
-			name = name.replaceAll(" ", "+");
-			String url = "http://deanclatworthy.com/imdb/?q=" + name;
-			System.out.println(url);
+			movieName = movieName.replaceAll(" ", "%20");
+
+			//1. Get the imdb JSON
 			HttpClient client = new DefaultHttpClient();
 			HttpGet request = new HttpGet(url);
 
@@ -62,48 +61,54 @@ public class Crawler {
 
 			StringBuffer result = new StringBuffer();
 			String line = "";
-			int c = 0;
 			while ((line = rd.readLine()) != null) {
 				result.append(line);
 			}
 
+			//2. Parse the JSON to get the imdbID
 			JSONParser jsonParser = new JSONParser();
-
 			Object obj = jsonParser.parse(result.toString());
-
 			JSONObject imdbMovieInfoJsonObject = (JSONObject) obj;
+			id = (String) imdbMovieInfoJsonObject.get("imdbID");
 
-			String imdbUrl = (String) imdbMovieInfoJsonObject.get("imdburl");
-			imdbUrl = imdbUrl.concat("reviews");
-
+			//3. Get the imdb url and return it.
+			String imdbUrl = "http://www.imdb.com/title/" + id + "/reviews";
 			System.out.println("Generated URL: " + imdbUrl);
 			return imdbUrl;
 
 		} catch (Exception e) {
 			e.printStackTrace();
-			return "Error";
 		}
+		return null;
 	}
 
-	public void retreiveReviewPage(String url, String name) {
+	/**
+	 * Gets the reviews from IMDB from the url.
+	 * Fetch the total number of review page.
+	 * then fetch the reviews from each page
+	 * */
+	public void retreiveReviewPage(String imdbMovieURL, String movieName) {
 		try {
 
-			if (url.equals("Error")) {
-				System.out.println("Done :D");
+			if (imdbMovieURL.equals("Error")) {
+				System.out.println("Some problem accured");
 				return;
 			}
-			System.out.println("Getting total number of review pages");
+			System.out.println("Getting the total number of review pages");
 
 			Document webpageDocument = null;
 
 			webpageDocument = Jsoup
-					.connect(url)
+					.connect(imdbMovieURL)
 					.timeout(10 * 1000)
 					.userAgent(
 							"Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36")
 					.get();
 
 			// to get total pages...each page has 10 reviews
+			// hard coded method to get the particular element where the reviews are stored.
+			//1. Getting the total number of review pages.
+			//          -View the imdb website for the DOM node which has the required information
 			org.jsoup.nodes.Element table = webpageDocument.getElementsByTag(
 					"table").get(1);
 			Iterator<org.jsoup.nodes.Element> itr = table.select("td")
@@ -111,23 +116,18 @@ public class Crawler {
 			int totalPages = Integer.parseInt(itr.next().text().split(" ")[3]
 					.replace(":", ""));
 			System.out.println(totalPages);
-			List<String> finalList = new ArrayList<>();
-			FileOutputStream fout;
-			PrintWriter pw = null;
 
-			fout = new FileOutputStream("D://project//imdbdata//reviews//"
-					+ name + ".txt");
-			pw = new PrintWriter(fout);
-
+			//2. Loop through each review page to get the reviews.
+			//   		-Every review page has same url but a trailing query paramter of start=<some number>
+			//			-Fetch the required DOM node which has the review in it.
 			for (int start = 0; start < totalPages; start++) {
-				// extracting reviews
 
 				System.out.println("Getting page: " + (start + 1));
 				int counter = 0;
 				List<String> reviewList = new ArrayList<>();
 				List<String> headingList = new ArrayList<>();
 
-				String modifiedUrl = url + "?start=" + (start * 10);
+				String modifiedUrl = imdbMovieURL + "?start=" + (start * 10);
 				webpageDocument = Jsoup
 						.connect(modifiedUrl)
 						.timeout(100 * 1000)
@@ -154,9 +154,9 @@ public class Crawler {
 				}
 
 				for (int i = 0; i < counter; i++) {
-					pw.println(headingList.get(i));
-					pw.println(reviewList.get(i));
-					pw.println();
+					System.out.println((headingList.get(i));
+					System.out.println(reviewList.get(i));
+					System.out.println();
 				}
 
 			}
